3a53cc1b74af8f821b80b3c68e7b3e52a211e012
Alla1965 <savanna.opt@gmail.com>
2026-03-17 20:37:28 +0200

    fix tailwind import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      screens: {
-        desktop: "1440px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
